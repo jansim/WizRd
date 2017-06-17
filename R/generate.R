e6a120b0e76778e365bbb26720668399e6c3c70e
Jan Simson <jan.simson@gmx.de>
2017-06-17 16:34:21 +0200

    Allow filtering of columns / use internal package main.Rmd

--- R/generate.R
+++ R/generate.R
@@ -1,11 +1,21 @@
 #' Generate a WizRd Report
 #'
 #' @param df Dataframe to generate the Report for
+#' @param pattern Pattern / Regex to use for subsettings the dataframe
+#' @param template Path to .Rmd template
 #'
 #' @export
 #'
 #' @examples
 #' generate(cars)
-generate <- function(df) {
-  render("inst/main.Rmd", params = list(set_title = "Test Bericht"))
+generate <- function(df, pattern = NULL, template = NULL) {
+  if (!is.null(pattern)) {
+    df <- df[,grep(pattern, names(df), value = T)]
+  }
+  
+  if (is.null(template)){
+    template <- system.file("main.Rmd", package = "WizRd")
+  } 
+  
+  render(template, params = list(set_title = "Test Bericht"))
 }
--- run.R
+++ run.R
@@ -6,5 +6,4 @@ source("R/render_child.R")
 
 load("data/gbd2015.RData")
 
-generate(gbd2015)
-
+generate(gbd2015, template = "inst/main.Rmd")
