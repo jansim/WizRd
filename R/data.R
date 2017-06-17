5d7c9fa7d7879d165a2aedf7b20db41a2ddd6e65
Jan Simson <jan.simson@gmx.de>
2017-06-17 15:16:40 +0200

    Added gbd2015 data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Data from the Global Burden of Disease Study 2015
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{cause_name}{medical cause / disease}
+#'   \item{cause_id}{individual id for each disease}
+#'   \item{metric_name}{class: number, rate or percent}
+#'   \item{acute_incidence}{acute incidence rate}
+#'   \item{chronic_prevalence}{chronic prevalence rate}
+#'   \item{DALYs}{disability adjusted life year - describes the numer of years lost due to ill-health, disability or early death}
+#'   \item{deaths}{number of deaths in the population}
+#'   \item{incidence}{number of incident cases in the population }
+#'   \item{prevalence}{prevalence rate}
+#'   \item{YLDs}{years lost due to disability}
+#'   \item{YLLs}{years of life lost due to dying early}
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"gbd2015"
