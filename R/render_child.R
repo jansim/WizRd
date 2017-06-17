eff6cb45701671c280124117486c1950bfc0e5ff
Jan Simson <jan.simson@gmx.de>
2017-06-17 16:00:26 +0200

    move render_child to seperate file

---
+++ R/render_child.R
@@ -0,0 +1,14 @@
+#' Render a child R Markdown document inside a parent R Markdown document.
+#' Has to run inside a .Rmd document!
+#'
+#' @param path Path to the .Rmd file (relative to current document)
+#'
+#' @export
+#'
+#' @examples
+#' ```{r, results='asis'}
+#' render.child("_general.Rmd")
+#' ```
+render_child <- function(path) {
+  cat(knit_child(path, quiet = T))
+}
